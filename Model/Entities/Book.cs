9d1d7b2d40dc7ff908c9435fbd21266466146f0a
Shinmengo <shinmengo222@gmail.com>
2020-01-29 14:11:04 +0100

    Dokumentacja cd

--- Model/Entities/Book.cs
+++ Model/Entities/Book.cs
@@ -9,12 +9,27 @@ namespace Model.Entities
 {
     public class Book
     {
+        /// <summary>
+        /// ID książki
+        /// </summary>
         public int ID { get; set; }
+        /// <summary>
+        /// Tytuł książki
+        /// </summary>
         [Display(Name = "Tytuł")]
         public string Name { get; set; }
+        /// <summary>
+        /// Ilośc kopii danej książki
+        /// </summary>
         [Display(Name = "Ilość")]
         public int Amount { get; set; }
+        /// <summary>
+        /// ID autora
+        /// </summary>
         public int AuthorID { get; set; }
+        /// <summary>
+        /// Dane autora
+        /// </summary>
         public virtual Author Author { get; set; }
     }
 }
--- Model/Entities/Borrower.cs
+++ Model/Entities/Borrower.cs
@@ -9,11 +9,23 @@ namespace Model.Entities
 {
     public class Borrower
     {
+        /// <summary>
+        /// ID wypożyczającego
+        /// </summary>
         public int ID { get; set; }
+        /// <summary>
+        /// Imie wypożyczającego
+        /// </summary>
         [Display(Name = "Imię")]
         public string FirstName { get; set; }
+        /// <summary>
+        /// Nazwisko wypożyczającego
+        /// </summary>
         [Display(Name = "Nazwisko")]
         public string LastName { get; set; }
+        /// <summary>
+        /// Aktywne wypożyczenia
+        /// </summary>
         public virtual ICollection<Loan> Loans { get; set; }
     }
 }
